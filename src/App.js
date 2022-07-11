7afb0bb98dfe2ca7673379020780e743b1c91f41
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-12 02:53:56 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 //import logo from './logo.svg';
 import './App.css';
-import Login from './pages/auth/login';
+//import Login from './pages/auth/login';
 //import Signup from './pages/auth/signup';
+import Registration from './pages/vendor/registration';
 
 function App() {
   return (
     <div className="App">
       
-       <Login></Login> 
-      {/* <Signup></Signup> */}
+       {/* <Login></Login> 
+      <Signup></Signup> */}
+      <Registration></Registration>
 
 
 
