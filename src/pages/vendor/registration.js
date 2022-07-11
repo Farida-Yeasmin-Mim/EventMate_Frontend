7bcfbd88664183089929c1b1fcd075d4037532f1
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-12 03:31:33 +0600

    Update registration.js

--- src/pages/vendor/registration.js
+++ src/pages/vendor/registration.js
@@ -1,20 +1,19 @@
 import * as React from 'react';
-import { Grid, Paper, Avatar, Button, FormControlLabel, TextField, Link, Typography, Checkbox} from '@mui/material';
+import { Grid, Paper, Avatar, Button, FormControlLabel, TextField, Link, Typography, Checkbox } from '@mui/material';
 import LockResetOutlinedIcon from '@mui/icons-material/LockResetOutlined';
 
 
-
 const Registration = () => {
-    const avatarStyle = { backgroundColor: '#ef73ee'}
+    const avatarStyle = { backgroundColor: '#ef73ee' }
     const paperStyle = { padding: 40, height: '110vh', width: 400, margin: '20px auto' }  //creating paperstyle variable by using const
-    const btnStyle = { margin: '20px 10px'}
+    const btnStyle = { margin: '20px 10px' }
 
-      
+
     return (
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                    <Avatar style={avatarStyle}><LockResetOutlinedIcon/></Avatar>
+                    <Avatar style={avatarStyle}><LockResetOutlinedIcon /></Avatar>
                     <h2>Registration Form</h2>
                     <p>Please fill in this form to create an account</p>
                 </Grid>
@@ -28,30 +27,28 @@ const Registration = () => {
                 <TextField id='standard-basic' label='Password' variant='standard' type='password' fullWidth required />
                 <TextField id='standard-basic' label='Confirm Password' variant='standard' type='password' fullWidth required />
 
-                
-                <br/>
-                <br/>
-                <FormControlLabel 
+
+                <br />
+                <br />
+                <FormControlLabel
                     control={
                         <Checkbox
-                            name = "checkedB"
-                            color= "primary"
+                            name="checkedB"
+                            color="primary"
                         />
                     }
-                    label= "I accept all the Terms of Use and Privacy Policy"
+                    label="I accept all the Terms of Use and Privacy Policy"
                 />
 
-                <Button type='submit' variant="contained" style= {btnStyle} fullWidth>Submit</Button>
-                
+                <Button type='submit' variant="contained" style={btnStyle} fullWidth>Submit</Button>
+
 
                 <Typography>Already a Member?
-                <Link href="#">Log In</Link>
+                    <Link href="#">Log In</Link>
                 </Typography>
 
             </Paper>
         </Grid>
-
-
 
     )
 }
