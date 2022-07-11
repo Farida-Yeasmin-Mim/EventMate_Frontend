6e3608c92c9c812e67f8aad9fafa002360355a67
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-12 03:21:42 +0600

    Update registration.js

--- src/pages/vendor/registration.js
+++ src/pages/vendor/registration.js
@@ -1,14 +1,11 @@
 import * as React from 'react';
-import { Grid, Paper, Avatar, TextField, Link, Typography} from '@mui/material';
-import Button from '@mui/material/Button';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { Grid, Paper, Avatar, Button, FormControlLabel, TextField, Link, Typography, Checkbox} from '@mui/material';
+import LockResetOutlinedIcon from '@mui/icons-material/LockResetOutlined';
 
 
 
-const Signup = () => {
-    const avatarStyle = { backgroundColor: '#cf88ce'}
+const Registration = () => {
+    const avatarStyle = { backgroundColor: '#ef73ee'}
     const paperStyle = { padding: 40, height: '110vh', width: 400, margin: '20px auto' }  //creating paperstyle variable by using const
     const btnStyle = { margin: '20px 10px'}
 
@@ -17,8 +14,8 @@ const Signup = () => {
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                    <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
-                    <h2>Sign Up</h2>
+                    <Avatar style={avatarStyle}><LockResetOutlinedIcon/></Avatar>
+                    <h2>Registration Form</h2>
                     <p>Please fill in this form to create an account</p>
                 </Grid>
 
@@ -26,13 +23,8 @@ const Signup = () => {
                 <TextField id='standard-basic' label='Name' variant='standard' fullWidth required />
                 <TextField id='standard-basic' label='Email' variant='standard' fullWidth required />
                 <TextField id='standard-basic' label='Address' variant='standard' fullWidth required />
-                <TextField id='standard-basic' label='Date of Birth' variant='standard' fullWidth required />
-
-               
                 <TextField id='standard-basic' label='Contact No.' variant='standard' type='contact' fullWidth required />
-                <TextField id='standard-basic' label='NID No.' variant='standard' fullWidth required />
-                
-
+                <TextField id='standard-basic' label='Licence No.' variant='standard' fullWidth required />
                 <TextField id='standard-basic' label='Password' variant='standard' type='password' fullWidth required />
                 <TextField id='standard-basic' label='Confirm Password' variant='standard' type='password' fullWidth required />
 
@@ -49,7 +41,7 @@ const Signup = () => {
                     label= "I accept all the Terms of Use and Privacy Policy"
                 />
 
-                <Button type='submit' variant="contained" style= {btnStyle} fullWidth>Sign Up</Button>
+                <Button type='submit' variant="contained" style= {btnStyle} fullWidth>Submit</Button>
                 
 
                 <Typography>Already a Member?
@@ -63,4 +55,4 @@ const Signup = () => {
 
     )
 }
-export default Signup;
+export default Registration;
