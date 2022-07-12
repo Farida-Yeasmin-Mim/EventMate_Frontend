a1cbedee58831e41149cdcd2e50be4aadbda454a
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-12 14:29:33 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@
 import './App.css';
 //import Login from './pages/auth/login';
 //import Signup from './pages/auth/signup';
-//import Registration from './pages/vendor/registration';
-import Navbar from './components/navbar';
-import DrawerComponent from './components/Drawer';
-import { BrowserRouter as Router} from "react-router-dom";
+import Registration from './pages/vendor/registration';
 
 function App() {
   return (
@@ -13,15 +10,7 @@ function App() {
 
       {/* <Login></Login> 
       <Signup></Signup> */}
-      {/* <Registration></Registration> */}
-      {/* <Navbar></Navbar> */}
-
-      <Router>
-      <Navbar />
-      
-    </Router>
-
-
+      <Registration></Registration>
 
 
 
