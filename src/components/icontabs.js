4b2c0a717eda27472e8cb57929ce279af4a2a08b
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-14 03:26:09 +0600

    Update icontabs.js

--- src/components/icontabs.js
+++ src/components/icontabs.js
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import FilterVintageTwoToneIcon from '@mui/icons-material/FilterVintageTwoTone';
+import MusicNoteTwoToneIcon from '@mui/icons-material/MusicNoteTwoTone';
+import CameraAltTwoToneIcon from '@mui/icons-material/CameraAltTwoTone';
+import FoodBankTwoToneIcon from '@mui/icons-material/FoodBankTwoTone';
+import BrushTwoToneIcon from '@mui/icons-material/BrushTwoTone';
+import PanToolTwoToneIcon from '@mui/icons-material/PanToolTwoTone';
+import CakeTwoToneIcon from '@mui/icons-material/CakeTwoTone';
+import WomanTwoToneIcon from '@mui/icons-material/WomanTwoTone';
 
-import PhoneMissedIcon from '@mui/icons-material/PhoneMissed';
 
 export default function IconTabs() {
   const [value, setValue] = React.useState(0);
@@ -12,20 +19,20 @@ export default function IconTabs() {
   };
 
   return (
+   
     <Tabs 
       value={value}
       onChange={handleChange}
       aria-label="icon position tabs example"
-    >
-    
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Decoration" />
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Music" />
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Photography" />
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Catering" />
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Makeover" />
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Mehedi" />
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Cake" />
-      <Tab icon={<PhoneMissedIcon />} iconPosition="start" label="Outfit" />
+    >  
+      <Tab icon={<FilterVintageTwoToneIcon />} iconPosition="start" label="Decoration" />
+      <Tab icon={<MusicNoteTwoToneIcon />} iconPosition="start" label="Music" />
+      <Tab icon={<CameraAltTwoToneIcon />} iconPosition="start" label="Photography" />
+      <Tab icon={<FoodBankTwoToneIcon />} iconPosition="start" label="Catering" />
+      <Tab icon={<BrushTwoToneIcon />} iconPosition="start" label="Makeover" />
+      <Tab icon={<PanToolTwoToneIcon />} iconPosition="start" label="Mehedi" />
+      <Tab icon={<CakeTwoToneIcon />} iconPosition="start" label="Cake" />
+      <Tab icon={<WomanTwoToneIcon />} iconPosition="start" label="Outfit" />
 
     </Tabs>
   );
