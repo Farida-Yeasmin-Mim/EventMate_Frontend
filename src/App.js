5e790f24f376ec0ecb2aacf1858cadd2b876b585
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-13 19:31:34 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 //import Login from './pages/auth/login';
 //import Signup from './pages/auth/signup';
 //import Registration from './pages/vendor/registration';
-import Navbar from './components/navbar';
+//import Navbar from './components/navbar';
+import Offer from './components/offer';
 
 function App() {
   return (
     <div className="App">
 
       {/* <Login></Login> 
-      {/* <Signup></Signup>
-      <Registration></Registration>  */}
-      <Navbar> </Navbar>
+      <Signup></Signup>
+       */}
+      {/* <Registration></Registration> 
+      <Navbar> </Navbar> */}
+
+      <Offer></Offer>
+
+
 
 
     </div>
