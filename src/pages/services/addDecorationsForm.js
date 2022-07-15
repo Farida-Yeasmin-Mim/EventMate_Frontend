f6ae0893e82977b330d5c37f9d365a917b3f9279
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-15 16:50:27 +0600

    Update addDecorationsForm.js

--- src/pages/services/addDecorationsForm.js
+++ src/pages/services/addDecorationsForm.js
@@ -1,37 +1,41 @@
 import * as React from 'react';
 import { Grid, Paper, Avatar, Button, FormControlLabel, TextField, Link, Typography, Checkbox} from '@mui/material';
-import LockResetOutlinedIcon from '@mui/icons-material/LockResetOutlined';
-import SelectTextFields from '.components/cards/selectTextField';
+import SelectTextFields from '../../components/selectTextField';
+import FilterVintageTwoToneIcon from '@mui/icons-material/FilterVintageTwoTone';
 
   
 const AddDecorationsForm = () => {
     const avatarStyle = { backgroundColor: '#ef73ee' }
-    const paperStyle = { padding: 40, height: '135vh', width: 410, margin: '20px auto' }  //creating paperstyle variable by using const
+    const paperStyle = { padding: 40, height: '175vh', width: 420, margin: '20px auto' }  //creating paperstyle variable by using const
     const btnStyle = { margin: '20px 10px' }
 
 
     return (
+
+        
         <Grid>
             <Paper elevation={10} style={paperStyle}>
                 <Grid align='center'>
-                    <Avatar style={avatarStyle}><LockResetOutlinedIcon /></Avatar>
+                    <Avatar style={avatarStyle}><FilterVintageTwoToneIcon /></Avatar>
                     <h2>Add Your Desire Decorations</h2>
                     <p>Please fill in this form to add decoration services</p>
-                </Grid>
+                </Grid> <br/>
 
 
-                <TextField id='standard-basic' label='Service Name' variant='standard' fullWidth/>
-                <TextField id='standard-basic' label='Email' variant='standard' fullWidth  />
-                <TextField id='standard-basic' label='Location' variant='standard' fullWidth  />
-                <TextField id='standard-basic' label='Contact No.' variant='standard' type='contact' fullWidth />
-                <TextField id='standard-basic' label='Max Guest No.' variant='standard' fullWidth/>
-                <TextField id='standard-basic' label='Required Space' variant='standard' fullWidth />
-                <TextField id='standard-basic' label='price' variant='standard' fullWidth/>
-                <TextField id='standard-basic' label='Inform Time' variant='standard' fullWidth/>
-                <TextField id='standard-basic' label='Build Time' variant='standard' fullWidth />
-                <TextField id='standard-basic' label='Offered Colour' variant='standard' fullWidth/>
+                <TextField id='standard-basic' label='Service Name' variant='standard' fullWidth/><br/><br/>
+                <TextField id='standard-basic' label='Location' variant='standard' fullWidth  /><br/><br/>
+                <TextField id='standard-basic' label='Contact No.' variant='standard' type='contact' fullWidth /><br/><br/>
+                <TextField id='standard-basic' label='Max Guest No.' variant='standard' fullWidth/><br/><br/>
+                <TextField id='standard-basic' label='Required Space' variant='standard' fullWidth /><br/><br/>
+                <TextField id='standard-basic' label='Price' variant='standard' fullWidth/><br/><br/>
+                <TextField id='standard-basic' label='Inform Time' variant='standard' fullWidth/><br/><br/>
+                <TextField id='standard-basic' label='Build Time' variant='standard' fullWidth /><br/><br/>
+                <TextField id='standard-basic' label='Offered Colour' variant='standard' fullWidth/><br/><br/>
+                <SelectTextFields/><br/>
                 <TextField id='standard-basic' label='Password' variant='standard' type='password' fullWidth />
-                <TextField id='standard-basic'><SelectTextFields/></TextField>
+                
+
+
 
                 
                 
@@ -45,7 +49,7 @@ const AddDecorationsForm = () => {
                             color="primary"
                         />
                     }
-                    label="I accept all the Terms of Use and Privacy Policy"
+                    label="I have checked everything clearly"
                 />
 
                 <Button type='submit' variant="contained" style={btnStyle} fullWidth>Submit</Button>
