c4a65e8210fde1c56eb97069ef1240d757ab56c1
Farida Yeasmin Mim <76836136+Farida-Yeasmin-Mim@users.noreply.github.com>
2022-07-29 18:39:25 +0600

    Update FilterPanel.js

--- src/components/event/filter/FilterPanel.js
+++ src/components/event/filter/FilterPanel.js
@@ -1,23 +1,69 @@
 import React from 'react'
 import FilterListToggle from '../../event/filter/FilterListToggle';
+import { categoryList, ratingList } from '../../event/filter/constants';
+import '../../event/filter/FilterPanel.css';
+import CheckboxProton from './CheckboxProton';
+import SliderProton from './SliderProton';
 
-const FilterPanel = ({categoryList, selectedCategory, selectToggle}) => {
-  return (
+
+const FilterPanel = ({ 
+  selectedCategory, 
+  selectToggle, 
+  selectedRating, 
+  selectRating, 
+  decoration, 
+  changeChecked,
+  changedPrice,
+  selectedPrice,
+}) => {
+  
+    return (
     <div>
-        {/* Category */}
-        <div className= "input-group">
-          <p className="label"> Category</p>
-          <FilterListToggle 
-          options ={categoryList} 
-          value={selectedCategory} 
-          selectToggle={selectToggle}/>
-        </div>
 
-        {/* Cuisine */}
+      {/* Category */}
+      <div className="input-group">
+        <p className="label"> Category</p>
+        <FilterListToggle
+          options={categoryList}
+          value={selectedCategory}
+          selectToggle={selectToggle} 
+          />
+      </div>
 
-        {/* Price Range */}
 
-        {/* Star Rating */}
+      {/* Decoration */}
+      <div className="input-group">
+        <p className="label"> Decoration</p>
+        {decoration.map((decoration)=>(
+        <CheckboxProton 
+        key={decoration.id}
+        decoration={decoration}
+        changeChecked={changeChecked} 
+        />
+        ))}
+      </div>
+
+
+      {/* Price Range */}
+      <div className="input-group">
+      <p className="label-range"> Star Rating</p>
+      <SliderProton value={selectedPrice} changedPrice={changedPrice}/>
+
+      </div>
+
+      
+      
+      {/* Star Rating */}
+      <div className="input-group">
+        <p className="label"> Star Rating</p>
+        <FilterListToggle
+          options={ratingList}
+          value={selectedRating}
+          selectToggle={selectRating} />
+      </div>
+
+
+
     </div>
   )
 }
